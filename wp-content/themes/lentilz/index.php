24ab22b4cf596eb95a7dc60a9bba68d87ea658e1
lentilz <lentieward@hotmail.com>
2019-01-26 17:18:50 -0500

    create 404 template

--- wp-content/themes/lentilz/index.php
+++ wp-content/themes/lentilz/index.php
@@ -14,4 +14,8 @@ $context = Timber::get_context();
 $context['posts'] = Timber::get_posts();
 $templates = array( 'index.twig' );
 
+if ( is_404() ) {
+  $templates = array( '404.twig' );
+}
+
 Timber::render( $templates, $context );
